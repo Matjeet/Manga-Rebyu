70b4e03455eba45552cdffc3accb460b6f1c1b38
Matjeet <matius29002@gmail.com>
2022-11-22 20:15:44 -0500

    feat: create new function 'crearIngresoManga'

--- DataLayerAPI/mangaRebyuDataManga.cs
+++ DataLayerAPI/mangaRebyuDataManga.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataLayerAPI
 {
-    internal class mangaRebyuDataManga
+    public class mangaRebyuDataManga
     {
+        private static SqlConnection conexion;
+
+        static mangaRebyuDataManga()
+        {
+            conexion = new SqlConnection(@"Password=Contraseña123;Persist Security Info=True;User ID=sa;Initial Catalog=mangaRebyu;Data Source=DESKTOP-HTIGAP3\SQLEXPRESS");
+        }
+        public static bool crearRegistroManga(string username, string manga, int rating, string coment)
+        {
+            conexion.Open();
+
+            string insert = string.Format(
+                "INSERT INTO Manga VALUES ('{0}','{1}', '{2}','{3}')",
+                username,
+                manga,
+                rating,
+                coment);
+
+            SqlCommand comando = new SqlCommand(insert, conexion);
+
+            int rows = comando.ExecuteNonQuery();
+
+            if (rows == 0)
+            {
+                conexion.Close();
+                return false;
+            }
+            else
+            {
+                conexion.Close();
+                return true;
+            }
+        }
     }
 }
