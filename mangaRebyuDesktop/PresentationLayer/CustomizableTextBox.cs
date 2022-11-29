b5e2d0bf7545a6343f5989f590c3d0d3c4f9bba3
Matjeet <matius29002@gmail.com>
2022-11-28 20:15:10 -0500

    feat: Customizable textBox final version

--- mangaRebyuDesktop/PresentationLayer/CustomizableTextBox.cs
+++ mangaRebyuDesktop/PresentationLayer/CustomizableTextBox.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace PresentationLayer
 {
@@ -19,6 +22,11 @@ namespace PresentationLayer
         private bool underLinedStyle = false;
         private Color borderFocusColor = Color.Red;
         private bool isFocused = false;
+        private int borderRadius = 0;
+        private Color placeholderColor = Color.DarkGray;
+        private string placeholderText = "";
+        private bool isPlaceholder = false;
+        private bool isPasswordChar = false;
 
         public CustomizableTextBox()
         {
@@ -53,15 +61,19 @@ namespace PresentationLayer
         [Category("Customizable TextBox")]
         public bool PasswordChar
         {
-            get { return textBox1.UseSystemPasswordChar; }
-            set { textBox1.UseSystemPasswordChar = value; }
+            get { return isPasswordChar; }
+            set 
+            { 
+                isPasswordChar = value;
+                textBox1.UseSystemPasswordChar = value; 
+            }
         }
 
         [Category("Customizable TextBox")]
         public bool Multiline
         {
             get { return textBox1.Multiline; }
-            set { textBox1.Multiline = false; }
+            set {textBox1.Multiline = false; }
         }
 
         [Category("Customizable TextBox")]
@@ -94,8 +106,16 @@ namespace PresentationLayer
         [Category("Customizable TextBox")]
         public string Texts
         {
-            get { return textBox1.Text; }
-            set { textBox1.Text = value; }
+            get 
+            {
+                if (isPlaceholder) return "";
+                else return textBox1.Text; 
+            }
+            set 
+            { 
+                textBox1.Text = value;
+                SetPlaceHolder();
+            }
         }
 
         [Category("Customizable TextBox")]
@@ -105,33 +125,156 @@ namespace PresentationLayer
             set { borderFocusColor = value; }
         }
 
+        [Category("Customizable TextBox")]
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set
+            {
+                if(value >= 0)
+                {
+                    borderRadius = value;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        public Color PlaceHolderColor
+        {
+            get { return placeholderColor; }
+            set
+            {
+                placeholderColor = value;
+                if (isPasswordChar)
+                    textBox1.ForeColor = value;
+            }
+        }
+
+        public string PlaceholderText
+        {
+            get { return placeholderText; }
+            set
+            {
+                placeholderText = value;
+                textBox1.Text = "";
+                SetPlaceHolder();
+            }
+        }
+
+        private void SetPlaceHolder()
+        {
+            if(string.IsNullOrWhiteSpace(textBox1.Text) && placeholderText != "")
+            {
+                isPlaceholder = true;
+                textBox1.Text = placeholderText;
+                textBox1.ForeColor = placeholderColor;
+                if(isPasswordChar)
+                    textBox1.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void RemovePlaceHolder()
+        {
+            if (isPlaceholder && placeholderText != "")
+            {
+                isPlaceholder = false;
+                textBox1.Text = "";
+                textBox1.ForeColor = this.ForeColor;
+                if (isPasswordChar)
+                    textBox1.UseSystemPasswordChar = true;
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             Graphics graph = e.Graphics;
-
             //Dibujar borde
-            using (Pen penBorder = new Pen(borderColor, borderSize))
+            if (borderRadius > 1)
             {
-                penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+                var rectBorderSmooth = this.ClientRectangle;
+                var rectBorder = Rectangle.Inflate(rectBorderSmooth, -borderSize, -borderSize);
+                int smoothSize = borderSize > 0 ? borderSize : 1;
 
-                if (!isFocused)
+                using (GraphicsPath pathBorderSmooth = GetFigurePath(rectBorderSmooth, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penBorderSmooth = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
                 {
-                    if (underLinedStyle)
-                        graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
-                    else
-                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
-                }
-                else
-                {
-                    penBorder.Color = borderFocusColor;
+                    this.Region = new Region(pathBorderSmooth);
+                    if (borderRadius > 15)
+                        SetTextBoxRoundedRegion();
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Center;
+
+                    if (isFocused) penBorder.Color = borderFocusColor;
 
                     if (underLinedStyle)
+                    {
+                        graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                        graph.SmoothingMode = SmoothingMode.None;
                         graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                    }
                     else
-                        graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    {
+                        graph.DrawPath(penBorderSmooth, pathBorderSmooth);
+                        graph.DrawPath(penBorder, pathBorder);
+                    }
                 }
             }
+            else
+            {
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    this.Region = new Region(this.ClientRectangle);
+                    penBorder.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
+
+                    if (!isFocused)
+                    {
+                        if (underLinedStyle)
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                        else
+                            graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+                    else
+                    {
+                        penBorder.Color = borderFocusColor;
+
+                        if (underLinedStyle)
+                            graph.DrawLine(penBorder, 0, this.Height - 1, this.Width, this.Height - 1);
+                        else
+                            graph.DrawRectangle(penBorder, 0, 0, this.Width - 0.5F, this.Height - 0.5F);
+                    }
+                }
+            }
+        }
+
+        private void SetTextBoxRoundedRegion()
+        {
+            GraphicsPath pathTxt;
+            if (Multiline)
+            {
+                pathTxt = GetFigurePath(textBox1.ClientRectangle, borderRadius - borderSize);
+                textBox1.Region = new Region(pathTxt);
+            }
+            else
+            {
+                pathTxt = GetFigurePath(textBox1.ClientRectangle, borderSize * 2);
+                textBox1.Region = new Region(pathTxt);
+            }
+        }
+
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
         }
 
         protected override void OnResize(EventArgs e)
@@ -190,12 +333,14 @@ namespace PresentationLayer
         {
             isFocused = true;
             this.Invalidate();
+            RemovePlaceHolder();
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
             isFocused = false;
             this.Invalidate();
+            SetPlaceHolder();
         }
     }
 }
--- mangaRebyuDesktop/PresentationLayer/Start.Designer.cs
+++ mangaRebyuDesktop/PresentationLayer/Start.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.customizableTextBox1 = new PresentationLayer.CustomizableTextBox();
             this.SuspendLayout();
+            // 
+            // customizableTextBox1
+            // 
+            this.customizableTextBox1.BackColor = System.Drawing.Color.White;
+            this.customizableTextBox1.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.customizableTextBox1.BorderFocusColor = System.Drawing.Color.Red;
+            this.customizableTextBox1.BorderRadius = 20;
+            this.customizableTextBox1.BorderSize = 2;
+            this.customizableTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.customizableTextBox1.Location = new System.Drawing.Point(200, 164);
+            this.customizableTextBox1.Multiline = false;
+            this.customizableTextBox1.Name = "customizableTextBox1";
+            this.customizableTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.customizableTextBox1.PasswordChar = true;
+            this.customizableTextBox1.PlaceHolderColor = System.Drawing.Color.DarkGray;
+            this.customizableTextBox1.PlaceholderText = "Password";
+            this.customizableTextBox1.Size = new System.Drawing.Size(250, 30);
+            this.customizableTextBox1.TabIndex = 0;
+            this.customizableTextBox1.Texts = "";
+            this.customizableTextBox1.UnderLinedStyle = false;
             // 
             // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customizableTextBox1);
             this.Name = "Start";
             this.Text = "Inicio";
             this.Load += new System.EventHandler(this.Start_Load);
@@ -43,5 +65,7 @@
         }
 
         #endregion
+
+        private CustomizableTextBox customizableTextBox1;
     }
 }
